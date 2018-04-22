199d15a6222ab6c8b7d673b846a078027b277e3a
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-22 06:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item)
     for (var i = 0; i < cart.length; i++) 
     { 
       if (cart[i].itemName === `${item}`) {
-        cart.splice(i,1, item);
+        cart.splice(i,1, `{$item}`);
       }
       else {return "That"
     }
